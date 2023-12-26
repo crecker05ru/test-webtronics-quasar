2c3dd0520f03ef841ae077eec6a1c80e69c07b0d
Anvar <fort2652@rambler.ru>
2023-12-26 21:15:31 +0300

    добавил комментарий

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="q-pa-md">
     <h2>Main</h2>
+    <p>В тз продублированный текст для страниц по пути "/" и "tickets"</p>
+    <p>Список tickets находится по роуту "tickets"нфкт</p>
   </div>
 </template>
 
